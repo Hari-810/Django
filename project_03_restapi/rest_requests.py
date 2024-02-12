25c0885c4f497ba14e000320976170912346c6c4
Hari-810 <harijee098@gmail.com>
2024-02-12 10:47:58 +0530

    model

--- project_03_restapi/rest_requests.py
+++ project_03_restapi/rest_requests.py
@@ -6,20 +6,20 @@ base_url = 'http://localhost:8000/api/tasks/'  # Replace with your API base URL
 response = requests.get(base_url)
 print(response.json())
 
-# GET - Retrieve a specific task by ID (Replace '1' with the desired task ID)
-task_id = 1
-response = requests.get(f'{base_url}{task_id}/')
-print(response.json())
+# # GET - Retrieve a specific task by ID (Replace '1' with the desired task ID)
+# task_id = 1
+# response = requests.get(f'{base_url}{task_id}/')
+# print(response.json())
 
-# POST - Create a new task
-new_task = {"title": "New Task", "description": "Task description"}
-response = requests.post(base_url, json=new_task)
-print(response.json())
+# # POST - Create a new task
+# new_task = {"title": "New Task", "description": "Task description"}
+# response = requests.post(base_url, json=new_task)
+# print(response.json())
 
-# PUT - Update an existing task by ID (Replace '1' with the desired task ID)
-updated_task = {"title": "Updated Task", "description": "Updated description"}
-response = requests.put(f'{base_url}{task_id}/', json=updated_task)
-print(response.json())
+# # PUT - Update an existing task by ID (Replace '1' with the desired task ID)
+# updated_task = {"title": "Updated Task", "description": "Updated description"}
+# response = requests.put(f'{base_url}{task_id}/', json=updated_task)
+# print(response.json())
 
-# DELETE - Delete a specific task by ID (Replace '1' with the desired task ID)
-response = requests.delete(f'{base_url}{task_id}/')
+# # DELETE - Delete a specific task by ID (Replace '1' with the desired task ID)
+# response = requests.delete(f'{base_url}{task_id}/')
